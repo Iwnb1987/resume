00ffc041df29d117ce1812937269ade27d12c1a0
Iwnb1987 <149844666+Iwnb1987@users.noreply.github.com>
2024-02-21 15:40:43 -0500

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+const h2 = document.getElementById('bth').style.color = "slateblue";
+
+const redFoot = document.querySelector('footer');
+redFoot.style.color = "red";
+
+const homeButton = document.getElementById('bth').style.color = '#39FF14';
+
+
+
